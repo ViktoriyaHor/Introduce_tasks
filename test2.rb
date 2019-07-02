e4062f93f48eec3b90d69cc2bcf3778b30820e9a
ViktoriyaHor <vhoroshun.face@gmail.com>
2019-07-02 13:20:16 +0300

    edit test2.rb not finish

--- test2.rb
+++ test2.rb
@@ -6,9 +6,9 @@ class Circle
     y = arg
     r_in = arg - 0.4
     r_out = arg + 0.4
-    while y >= -arg do
+    while y >= -arg
       x = -arg
-      while x <= r_out do
+      while x <= r_out
         if (x * x + y * y >= r_in * r_in) && (x * x + y * y != r_out * r_out)
           print ' '
         else
@@ -21,4 +21,3 @@ class Circle
     end
   end
 end
-
